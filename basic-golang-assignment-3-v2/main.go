1cf38989910d2a147b248d2a55ff8944511034cd
septianhari <aribarno86@gmail.com>
2024-03-29 20:58:51 +0700

    menyelesaikan FP-3

--- basic-golang-assignment-3-v2/main.go
+++ basic-golang-assignment-3-v2/main.go
@@ -5,7 +5,12 @@ import (
 	"fmt"
 )
 
-// StudentManager merupakan interface yang mendefinisikan fungsi-fungsi untuk student portal.
+type Student struct {
+	ID           string
+	Name         string
+	StudyProgram string
+}
+
 type StudentManager interface {
 	GetStudents() []Student
 	Login(id, name string) (string, error)
@@ -14,43 +19,33 @@ type StudentManager interface {
 	ModifyStudent(name string, fn StudentModifier) (string, error)
 }
 
-// StudentModifier merupakan tipe fungsi yang digunakan untuk memodifikasi data mahasiswa.
-type StudentModifier func(student *Student) error
-
-// Student adalah struktur data untuk merepresentasikan seorang mahasiswa.
-type Student struct {
-	ID           string
-	Name         string
-	StudyProgram string
-}
-
-// InMemoryStudentManager adalah implementasi dari StudentManager yang menggunakan penyimpanan dalam memori.
 type InMemoryStudentManager struct {
 	students             []Student
 	studentStudyPrograms map[string]string
 }
 
-// NewInMemoryStudentManager membuat instans baru dari InMemoryStudentManager.
-func NewInMemoryStudentManager() *InMemoryStudentManager {
-	// Contoh inisialisasi map program studi mahasiswa
-	studentStudyPrograms := map[string]string{
-		"SI": "Sistem Informasi",
-		"TK": "Teknik Komputer",
-		"TI": "Teknik Informatika",
-	}
+type StudentModifier func(*Student) error
 
+func NewInMemoryStudentManager() *InMemoryStudentManager {
 	return &InMemoryStudentManager{
-		students:             []Student{},
-		studentStudyPrograms: studentStudyPrograms,
+		students: []Student{
+			{ID: "A12345", Name: "Aditira", StudyProgram: "TI"},
+			{ID: "B21313", Name: "Dito", StudyProgram: "TK"},
+			{ID: "A34555", Name: "Afis", StudyProgram: "MI"},
+		},
+		studentStudyPrograms: map[string]string{
+			"SI": "Sistem Informasi",
+			"TK": "Teknik Komputer",
+			"TI": "Teknik Informatika",
+			"MI": "Manajemen Informatika",
+		},
 	}
 }
 
-// GetStudents mengembalikan seluruh data mahasiswa.
 func (ism *InMemoryStudentManager) GetStudents() []Student {
 	return ism.students
 }
 
-// Login memeriksa apakah data mahasiswa terdapat dalam slice students.
 func (ism *InMemoryStudentManager) Login(id, name string) (string, error) {
 	if id == "" || name == "" {
 		return "", errors.New("ID or Name is undefined!")
@@ -61,18 +56,16 @@ func (ism *InMemoryStudentManager) Login(id, name string) (string, error) {
 			return fmt.Sprintf("Login berhasil: %s", name), nil
 		}
 	}
-
 	return "", errors.New("Login gagal: data mahasiswa tidak ditemukan")
 }
 
-// Register menambahkan data mahasiswa baru ke dalam slice students.
 func (ism *InMemoryStudentManager) Register(id, name, studyProgram string) (string, error) {
 	if id == "" || name == "" || studyProgram == "" {
 		return "", errors.New("ID, Name or StudyProgram is undefined!")
 	}
 
-	if _, ok := ism.studentStudyPrograms[studyProgram]; !ok {
-		return "", fmt.Errorf("Study program %s is not found", studyProgram)
+	if _, exists := ism.studentStudyPrograms[studyProgram]; !exists {
+		return "", errors.New(fmt.Sprintf("Study program %s is not found", studyProgram))
 	}
 
 	for _, student := range ism.students {
@@ -82,23 +75,22 @@ func (ism *InMemoryStudentManager) Register(id, name, studyProgram string) (stri
 	}
 
 	ism.students = append(ism.students, Student{ID: id, Name: name, StudyProgram: studyProgram})
-	return fmt.Sprintf("Registrasi berhasil: %s (%s)", name, ism.studentStudyPrograms[studyProgram]), nil
+	return fmt.Sprintf("Registrasi berhasil: %s (%s)", name, studyProgram), nil
 }
 
-// GetStudyProgram mencari nama program studi dari map studentStudyPrograms berdasarkan kode program studi yang dimasukkan.
 func (ism *InMemoryStudentManager) GetStudyProgram(code string) (string, error) {
 	if code == "" {
 		return "", errors.New("Code is undefined!")
 	}
 
-	if program, ok := ism.studentStudyPrograms[code]; ok {
-		return program, nil
+	program, exists := ism.studentStudyPrograms[code]
+	if !exists {
+		return "", errors.New("Kode program studi tidak ditemukan")
 	}
 
-	return "", errors.New("Kode program studi tidak ditemukan")
+	return program, nil
 }
 
-// ModifyStudent mencari data mahasiswa berdasarkan nama yang dimasukkan, dan akan memodifikasinya menggunakan fungsi fn.
 func (ism *InMemoryStudentManager) ModifyStudent(name string, fn StudentModifier) (string, error) {
 	if name == "" {
 		return "", errors.New("Mahasiswa tidak ditemukan.")
@@ -116,10 +108,9 @@ func (ism *InMemoryStudentManager) ModifyStudent(name string, fn StudentModifier
 	return "", errors.New("Mahasiswa tidak ditemukan.")
 }
 
-// ChangeStudyProgram mengembalikan sebuah fungsi yang akan memodifikasi program studi dari mahasiswa yang diberikan.
 func (ism *InMemoryStudentManager) ChangeStudyProgram(programStudi string) StudentModifier {
 	return func(student *Student) error {
-		if _, ok := ism.studentStudyPrograms[programStudi]; !ok {
+		if _, exists := ism.studentStudyPrograms[programStudi]; !exists {
 			return errors.New("Kode program studi tidak ditemukan")
 		}
 		student.StudyProgram = programStudi
