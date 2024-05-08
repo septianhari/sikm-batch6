99350d8ba2f71e252898c91e59c911cd43fbb5b4
septianhari <aribarno86@gmail.com>
2024-05-08 07:41:03 +0700

    cicil

---
+++ web-app-assignment-1-v2/repository/category.go
@@ -0,0 +1,63 @@
+package repository
+
+import (
+	"a21hc3NpZ25tZW50/db/filebased"
+	"a21hc3NpZ25tZW50/model"
+)
+
+type CategoryRepository interface {
+	Store(Category *model.Category) error
+	Update(id int, category model.Category) error
+	Delete(id int) error
+	GetByID(id int) (*model.Category, error)
+	GetList() ([]model.Category, error)
+}
+
+type categoryRepository struct {
+	filebasedDb *filebased.Data
+}
+
+func NewCategoryRepo(filebasedDb *filebased.Data) CategoryRepository {
+	return &categoryRepository{filebasedDb}
+}
+
+func (c *categoryRepository) Store(Category *model.Category) error {
+	err := c.filebasedDb.StoreCategory(*Category)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (c *categoryRepository) Update(id int, category model.Category) error {
+	category.ID = id
+	err := c.filebasedDb.UpdateCategory(id, category)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (c *categoryRepository) Delete(id int) error {
+	err := c.filebasedDb.DeleteCategory(id)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (c *categoryRepository) GetByID(id int) (*model.Category, error) {
+	category, err := c.filebasedDb.GetCategoryByID(id)
+	if err != nil {
+		return nil, err
+	}
+	return category, nil
+}
+
+func (c *categoryRepository) GetList() ([]model.Category, error) {
+	categories, err := c.filebasedDb.GetCategories()
+	if err != nil {
+		return nil, err
+	}
+	return categories, nil
+}
