71dba83275449a3f343e4927c88505f76d46dd39
septianhari <aribarno86@gmail.com>
2024-05-20 10:22:58 +0700

    mengenal postgres

---
+++ exercise-db-1-v1/main.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"crypto/sha256"
+	"database/sql"
+	"fmt"
+	"io/ioutil"
+	"log"
+
+	_ "github.com/lib/pq"
+)
+
+type DBCredential struct {
+	HostName     string
+	DatabaseName string
+	Username     string
+	Password     string
+	Port         string
+}
+
+// TODO: masukkan CAMP_ID kalian dan Credential Database kalian disini
+var (
+	CAMP_ID = "BE8172509" // TODO: replace this
+
+	credential = DBCredential{
+		HostName:     "localhost",
+		DatabaseName: "rg_database",
+		Username:     "postgres",
+		Password:     "postgres",
+		Port:         "5432",
+	}
+)
+
+func Connection() (db *sql.DB, err error) {
+	//setup connection to database postgres
+	psqlInfo := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
+		credential.HostName, credential.Port, credential.Username, credential.Password, credential.DatabaseName)
+
+	db, err = sql.Open("postgres", psqlInfo)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = db.Ping()
+	if err != nil {
+		return
+	} else {
+		fmt.Println("Successfully connected!")
+		err = ioutil.WriteFile("output.txt", []byte(CAMP_ID+" "+fmt.Sprintf("%x", sha256.Sum256([]byte(CAMP_ID)))), 0644)
+		return db, err
+	}
+}
+
+func main() {
+	_, err := Connection()
+
+	if err != nil {
+		log.Fatal(err)
+	}
+}
