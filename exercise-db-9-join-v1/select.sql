5d906cf99f7f0701adc86fd0480bd710d526dd2a
septianhari <aribarno86@gmail.com>
2024-05-22 14:54:15 +0700

    db-9-join

---
+++ exercise-db-9-join-v1/select.sql
@@ -0,0 +1,19 @@
+-- TODO: answer here
+SELECT 
+    reports.id, 
+    students.fullname, 
+    students.class, 
+    students.status, 
+    reports.study, 
+    reports.score 
+FROM 
+    reports 
+RIGHT JOIN 
+    students 
+ON 
+    reports.student_id = students.id 
+WHERE 
+    students.status = 'active' 
+    AND reports.score < 70 
+ORDER BY 
+    reports.score ASC;
